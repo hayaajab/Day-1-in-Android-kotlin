d8a3ac057eb54152c7be2f6c08ff6340b8268d93
hayaajab <52868771+hayaajab@users.noreply.github.com>
2023-01-01 18:55:58 +0300

    Add files via upload

---
+++ Haya.kt
@@ -0,0 +1,14 @@
+package com.example.myfirstapplication
+
+fun main() {
+    val A:String = "Abdullah"
+    val B:Float = 20.6F
+    val C:Double = 8.9746325
+    val D:Byte = 25
+    val F:Short = 2022
+    val J:Long = 321984526954
+    val K:Boolean = true
+    val M:Char = 'A'
+
+
+}
